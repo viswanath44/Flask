796b3b367213fa98db667a8be4bc7632c7cc257d
Viswanath <cloudshare2k17@gmail.com>
2023-09-18 23:27:57 -0500

    Basic addition Flask

---
+++ main.py
@@ -0,0 +1,39 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+# Importing flask module in the project is mandatory
+# An object of Flask class is our WSGI application.
+# from flask import Flask
+#
+# app = Flask(__name__)
+#
+# # The route() function of the Flask class is a decorator,
+# # which tells the application which URL should call
+# # the associated function.
+# @app.route('/displayContent')
+# # ‘/’ URL is bound with hello_world() function.
+# def hello_world():
+# 	return 'Hello World'
+#
+# # main driver function
+# if __name__ == '__main__':
+#
+# 	# run() method of Flask class runs the application
+# 	# on the local development server.
+# 	app.run()
+
+
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
